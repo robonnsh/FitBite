924e07e47a79d21ce522c1d08cc14b010a7489ed
robo ninoshvili <nnshtofficial@gmail.com>
2024-05-11 01:28:22 +0400

    add options class for user to choose different kind of calculators

---
+++ Options.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitBite
+{
+    public class Options
+    {
+        public void ChooseOption(UserInfo userInfo)
+        {
+            Console.WriteLine($"{userInfo.Weight} {userInfo.Height}");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("");
+            Console.WriteLine("1. Calculate BMI");
+            Console.WriteLine("");
+            Console.WriteLine("2. Calculate Daily Protain Dose");
+            int option = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            switch (option)
+            {
+                case 1:
+                    CalcBMI(userInfo);
+                    break;
+                case 2:
+                    CalcProtein(userInfo);
+                    break;
+                default:
+                    Console.WriteLine("Invalid option");
+                    break;
+            }
+        }
+
+
+        private void CalcBMI(UserInfo userinfo)
+        {
+            BmiCalculator calcBMI = new BmiCalculator();
+            double bmi = calcBMI.CalcBMI(userinfo.Weight, userinfo.Height);
+            Console.WriteLine($"Your BMI is: {bmi}");
+            string bmiLevels = calcBMI.BmiLevels(bmi);
+            Console.WriteLine("");
+            Console.WriteLine($"*** World Health Organization's recommended body\nweight" +
+                $" based on your BMI value is --{bmiLevels}--");
+            Console.WriteLine("");
+        }
+
+        private void CalcProtein(UserInfo userInfo)
+        {
+            ProteinCalculator proteinCalc = new ProteinCalculator();
+            double dailyProtein = proteinCalc.CalcDailyProtein(userInfo.Weight, userInfo.Height,
+                userInfo.Age, userInfo.Gender, userInfo.Activity, userInfo.weightGoal );
+            Console.WriteLine($"*** Your Daily Protein Dose is {dailyProtein}");
+        }
+    }    
+
+}
+
+
